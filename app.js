e219f452408c1a86ede40a0b95f1a67727b87da8
Prasenjit Aluni <prasenjit.aluni@codeclouds.com>
2024-01-17 19:10:46 +0530

    Onito

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ console.log('\n\n-: App Started :-');
 const express   = require('express');
 const app       = express();
 
+
+
 app.use('/', (req, res, next)=>{
+
+    let arr = [];
+    for(let i = 0; i<3; i++){
+        for(let j = 0; j<9; j++){
+            arr[i][j] = rand(0,9);
+        }
+    }
+    console.log(arr);
+
     console.log('-: Welcome :-');
-    res.send('-: Welcome :-');
+    res.render('onito', {data:arr});
     next();
 });
 
